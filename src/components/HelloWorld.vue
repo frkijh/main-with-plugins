b31a6c5f66eb251fa5208394570ef1299f451858
wangmengyuan <mengyuan.wang@primelifescience.com.cn>
2026-03-23 17:50:24 +0800

    look the difference between two repository

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@ const count = ref(0)
       <img :src="viteLogo" class="vite" alt="Vite logo" />
     </div>
     <div>
-      <h1>Get started</h1>
+      <h1>1114514 Get started</h1>
       <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
     </div>
     <button class="counter" @click="count++">Count is {{ count }}</button>
